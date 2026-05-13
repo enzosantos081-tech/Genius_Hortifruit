897e8646e85ac81fdda0ce77995ff733ff2557b0
enzosantos081-tech <enzo.santos081@al.unieduk.com.br>
2026-05-13 19:33:03 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ import os
 from datetime import datetime
 
 app = Flask(__name__)
-CORS(app, origins=["http://127.0.0.1:5500", "http://localhost:5500"])
+CORS(app, origins=["https://enzosantos081-tech.github.io"])
 
 # ============================================================
 # Caminhos dos arquivos JSON
